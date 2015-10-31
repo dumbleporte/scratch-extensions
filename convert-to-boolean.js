8fe4b24c4b2c03f2307a23fb2c041cd6e2d25f9c
Sam <savaka2@users.noreply.github.com>
2015-10-31 17:00:48 -0400

    Create Convert to Boolean extension
    New extension to report string input as boolean

---
+++ convert-to-boolean.js
@@ -0,0 +1,32 @@
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			['b', '%s as boolean', 'asboolean'],
+			['b', 'report %s', 'report']
+		],
+		url : "https://github.com/savaka2/scratch-extensions/wiki/Convert-to-Boolean-extension"
+	};
+	
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+	
+	ext.asboolean = function(s) {
+		if (s = true || s = 'true') {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
+	ext.report = function(s) {
+		return s;
+	};
+	
+	ScratchExtensions.register('Convert to Boolean', descriptor, ext);
+})();
