33dbeeba4e3976b8a494e51529e49d578e7c2a31
Sam <savaka2@users.noreply.github.com>
2015-11-02 18:25:02 -0500

    Add run reporters
    Replaces run blocks removed from JavaScript and more math extension
    (also fix missing comma)

---
+++ run-reporters.js
@@ -0,0 +1,22 @@
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			[' ', 'run %s', 'do']
+		],
+		url: "https://github.com/savaka2/scratch-extensions/wiki/Run-Reporters-extension"
+	};
+	
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+	ext.do = function(string) {
+		useless = string;
+	};
+	
+	ScratchExtensions.register('Run Reporters', descriptor, ext);
+})();
