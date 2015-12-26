b34f0fb643f306012cab645cbd1a2d1ed572bf2a
Sam <savaka2@users.noreply.github.com>
2015-12-26 08:13:00 -0500

    Add jQuery there

--- scratch-api.js
+++ scratch-api.js
@@ -1,5 +1,10 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+xhr.onload = function() {
+	eval(xhr.response);
+
+
 new (function() {
-	//INCLUSION OF JQUERY NEEDED
 	// THIS EXTENSION IS NOT FINISHED
 	var ext = this;
 	var descriptor = {
@@ -39,3 +44,7 @@ new (function() {
 	
 	ScratchExtensions.register('Scratch API', descriptor, ext);
 })();
+
+
+}
+xhr.send();
