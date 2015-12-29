2dab869224548250362506d3b3874c9c1ee0678d
Sam <savaka2@users.noreply.github.com>
2015-12-29 18:11:32 -0500

    Add console log thing

--- cloud-variable-server.js
+++ cloud-variable-server.js
@@ -25,6 +25,7 @@ new (function() {
 					list.push((cvars[i]).name);
 				}
 				var answer = (cvars[list.indexOf('☁ ' + varname)]).value;
+				console.log(answer);
 				if (Number(answer) != NaN) {
 					return Number(answer);
 				} else {
