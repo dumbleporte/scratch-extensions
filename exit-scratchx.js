f9a70672053904d7219a11d4830ac81b5a09a8f6
Sam <savaka2@users.noreply.github.com>
2016-01-05 10:29:12 -0500

    Create Exit ScratchX

---
+++ exit-scratchx.js
@@ -0,0 +1,21 @@
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			[' ', 'exit scratchx', 'exit']
+		],
+		url : "https://github.com/savaka2/scratch-extensions/wiki/Exit-ScratchX-extension"
+	};
+	
+	ext._shutdown = function() {};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+	
+	ext.exit = function() {
+		
+	};
+	
+	ScratchExtensions.register('Exit ScratchX', descriptor, ext);
+})();
