6566f1dcce25afe4c227f3ea54ab9c8d00c03012
Sam <savaka2@users.noreply.github.com>
2016-01-12 13:36:17 -0500

    Use crossorigin.me

--- github-status-api.js
+++ github-status-api.js
@@ -22,7 +22,7 @@ new (function() {
 	};
 	
 	ext.currentstatus = function(stuff, callback) {
-		var jsonurl = 'https://status.github.com/api/status.json';
+		var jsonurl = 'https://crossorigin.me/https://status.github.com/api/status.json';
 		var r = new XMLHttpRequest();
 		r.addEventListener('load', function() {
 			if (r.responseText) {
@@ -44,7 +44,7 @@ new (function() {
 	} // Credit to Zatnik
 	
 	ext.lastmessage = function(stuff, callback) {
-		var jsonurl = 'https://status.github.com/api/last-message.json';
+		var jsonurl = 'https://crossorigin.me/https://status.github.com/api/last-message.json';
 		var r = new XMLHttpRequest();
 		r.addEventListener('load', function() {
 			if (r.responseText) {
