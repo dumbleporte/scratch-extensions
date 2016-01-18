4812fd720fc3c55ab23fc436c0511fa09295d134
Sam <savaka2@users.noreply.github.com>
2016-01-18 14:16:49 -0500

    Delete debug things

--- cloud-variable-server.js
+++ cloud-variable-server.js
@@ -19,15 +19,12 @@
 		r.addEventListener("load", function() {
 			if (r.responseText) {
 				var obj = JSON.parse(r.responseText);
-				console.log(obj);
 				var cvars = obj.variables;
 				var list = [];
 				for (i = 0; i < cvars.length; i++) {
 					list.push((cvars[i]).name);
 				}
-				console.log(list);
 				var answer = (cvars[list.indexOf('☁ ' + varname)]).value;
-				console.log(answer);
 				if (Number(answer) != NaN) {
 					callback(Number(answer));
 				} else {
