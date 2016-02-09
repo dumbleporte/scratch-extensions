8dbe1e93bb642f20f62d38a53b4a3d5a54104645
Sam <savaka2@users.noreply.github.com>
2016-02-09 12:03:47 -0500

    Update data-records.js

--- data-records.js
+++ data-records.js
@@ -20,11 +20,39 @@ new (function() {
 	};
 	
 	ext.objInit = function(name, keys) {
-		//code
+		var pos = -1;
+		for (i = 0; i < objects.length; i++) {
+			if (objects[i].nameText == name) {
+				pos = i;
+				break;
+			}
+		}
+		var creating = {};
+		creating.nameText = name;
+		var splitKeys = keys.split(' ');
+		var creatingData = {};
+		for (i = 0; i < splitKeys.length; i++) {
+			creatingData[splitKeys[i]] = '';
+		}
+		creating.data = creatingData;
+		if (pos != -1) {
+			objects[pos] = creating;
+		} else {
+			objects.push(creating);
+		}
 	};
 	
 	ext.objSet = function(key, name, value) {
-		//code
+		var pos = -1;
+		for (i = 0; i < objects.length; i++) {
+			if (objects[i].nameText == name) {
+				pos = i;
+				break;
+			}
+		}
+		if (pos != -1 && objects[pos].data[key] != undefined) {
+			objects[pos].data[key] = value; //idk
+		}
 	};
 	
 	ext.objGet = function(key, name) {
