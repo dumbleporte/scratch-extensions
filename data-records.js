205b96487fb5d199559c39ba931d7f2910c4d189
Sam <savaka2@users.noreply.github.com>
2016-02-09 16:16:34 -0500

    Update data-records.js

--- data-records.js
+++ data-records.js
@@ -2,24 +2,40 @@ new (function() {
 	var ext = this;
 	var descriptor = {
 		blocks: [
-			[' ', 'initialize object %s with parameters %s (comma separated)', 'objInit'],
+			[' ', 'initialize object %s with fields %s (comma separated)', 'objInit'],
 			[' ', 'set %s of object %s to %s', 'objSet'],
-			['r', '%s of object %s', 'objGet', '']
+			['r', '%s of object %s', 'objGet', ''],
+			['-'],
+			[' ', 'initialize table %s with columns %s (comma separated)', 'tableInit'],
+			[' ', 'add row with data %s (comma separated) to table %s', 'tableAdd'],
+			[' ', 'delete row %n of table %s', 'tableDel'],
+			[' ', 'delete all rows of table %s', 'tableClear'],
+			[' ', 'insert row at position %n with data %s (comma separated) of table %s', 'tableIns'],
+			[' ', 'set %s of row %n of table %s to %s', 'tableSet'],
+			['r', '%s of row %n of table %s', 'tableGet', ''],
+			['r', 'length of table %s', 'tableGet', '']
+			['-'],
+			['r', 'properties of %m.types %s', 'r', ''],
 		],
+		menus: {
+			'types': ['object', 'table', 'index']
+		}
 		url : 'https://github.com/savaka2/scratch-extensions/wiki/Data-Records-extension'
 	};
 	
 	objects = [];
+	tables = [];
 	
 	ext._shutdown = function() {
 		objects = [];
+		tables = [];
 	};
 	
 	ext._getStatus = function() {
 		return {status:2, msg:'Ready'};
 	};
 	
-	ext.objInit = function(name, keys) {
+	ext.objInit = function(name, fields) {
 		var pos = -1;
 		for (i = 0; i < objects.length; i++) {
 			if (objects[i].nameText == name) {
@@ -29,10 +45,10 @@ new (function() {
 		}
 		var creating = {};
 		creating.nameText = name;
-		var splitKeys = keys.split(',');
+		var splitFields = fields.split(',');
 		var creatingData = {};
-		for (i = 0; i < splitKeys.length; i++) {
-			creatingData[splitKeys[i]] = '';
+		for (i = 0; i < splitFields.length; i++) {
+			creatingData[splitFields[i]] = '';
 		}
 		creating.data = creatingData;
 		if (pos != -1) {
@@ -42,7 +58,7 @@ new (function() {
 		}
 	};
 	
-	ext.objSet = function(key, name, value) {
+	ext.objSet = function(field, name, value) {
 		var pos = -1;
 		for (i = 0; i < objects.length; i++) {
 			if (objects[i].nameText == name) {
@@ -50,12 +66,12 @@ new (function() {
 				break;
 			}
 		}
-		if (pos != -1 && objects[pos].data[key] != undefined) {
-			objects[pos].data[key] = value; //idk
+		if (pos != -1 && objects[pos].data[field] != undefined) {
+			objects[pos].data[field] = value; //idk
 		}
 	};
 	
-	ext.objGet = function(key, name) {
+	ext.objGet = function(field, name) {
 		var pos = -1;
 		for (i = 0; i < objects.length; i++) {
 			if (objects[i].nameText == name) {
@@ -63,8 +79,8 @@ new (function() {
 				break;
 			}
 		}
-		if (pos != -1 && objects[pos].data[key] != undefined) {
-			return objects[pos].data[key];
+		if (pos != -1 && objects[pos].data[field] != undefined) {
+			return objects[pos].data[field];
 		} else {
 			return '';
 		}
