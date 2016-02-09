e8f7a35f9795b5cad44030a47789378c1c9d6754
Sam <savaka2@users.noreply.github.com>
2016-02-08 19:46:39 -0500

    more

--- data-records.js
+++ data-records.js
@@ -2,7 +2,9 @@ new (function() {
 	var ext = this;
 	var descriptor = {
 		blocks: [
-			[' ', 'initialize object %s', 'objInit']
+			[' ', 'initialize object %s with parameters %s (space separated)', 'objInit'],
+			[' ', 'set %s of object %s to %s', 'objSet'],
+			['r', '%s of object %s', 'objGet', ''],
 		],
 		url : 'https://github.com/savaka2/scratch-extensions/wiki/Data-Records-extension'
 	};
@@ -17,7 +19,15 @@ new (function() {
 		return {status:2, msg:'Ready'};
 	};
 	
-	ext.objInit = function(s) {
+	ext.objInit = function(name, keys) {
+		//code
+	};
+	
+	ext.objSet = function(key, name, value) {
+		//code
+	};
+	
+	ext.objGet = function(key, name) {
 		//code
 	};
 	
