bf4a28816e3ffaf8349b22033c111618747a0d93
Sam <savaka2@users.noreply.github.com>
2016-03-02 09:48:31 -0500

    fixes and add news blocks
    (save)

--- scratch-api.js
+++ scratch-api.js
@@ -2,19 +2,23 @@ new (function() {
 	var ext = this;
 	var descriptor = {
 		blocks: [/* not made yet
-			['R', 'following %n of user %s', 'userUsernameFollowing1', 'mres'],
+			['R', '%n most recent following of user %s', 'userUsernameFollowing1', 'mres'],
 			['R', 'following count of user %s', 'userUsernameFollowing2', 'mres'],
 			['B', 'user %s is following %s?', 'userUsernameFollowing3', 'mres'],
-			['R', 'follower %n of user %s', 'userUsernameFollowers1', 'mres'],
+			['R', '%n most recent follower of user %s', 'userUsernameFollowers1', 'mres'],
 			['R', 'follower count of user %s', 'userUsernameFollowers2', 'mres'],
 			['B', 'user %s has follower %s?', 'userUsernameFollowers3', 'mres'],
 			['-'],*/
 			['R', 'message count of user %s', 'userUsernameMessagesCount', 'mres'],
 			['-'],
+			['R', '%s of news item %n', 'news1', 'title', 1],
+			['R', 'length of news', 'news2', ''],
+			['-'],
 			['R', '%m.users of user %s', 'userUsername', 'id', 'mres']
 		],
 		menus: {
-			users: ['id', 'join time', 'country', 'about me', 'what I\'m working on']
+			users: ['id', 'join time', 'country', 'about me', 'what I\'m working on'],
+			news: ['post id', 'time', 'title', 'link', 'text']
 		},
 		url: 'https://github.com/savaka2/scratch-extensions/wiki/Scratch-API-extension'
 	};
@@ -25,13 +29,81 @@ new (function() {
 		return {status:2, msg:'Ready'};
 	};
 	
+	ext.userUsernameMessagesCount = function(user, callback) {
+		var jsonurl = 'https://api.scratch.mit.edu/users/' + user + '/messages/count?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
+		var r = new XMLHttpRequest();
+		r.addEventListener('load', function() {
+			if (r.responseText) {
+				var obj = JSON.parse(r.responseText);
+				callback(obj.count);
+			} else {
+				callback('');
+			}
+		});
+		r.addEventListener('error', function() {callback('')});
+		r.open('get', jsonurl, true);
+		r.send();
+	}
+	
+	ext.news1 = function(stuff, number, callback) {
+		var jsonurl = 'https://api.scratch.mit.edu/news?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
+		var r = new XMLHttpRequest();
+		r.addEventListener('load', function() {
+			if (r.responseText) {
+				var obj = JSON.parse(r.responseText);
+				var post = obj[number - 1];
+				if (post != undefined) {
+					switch (stuff) {
+						case 'post id':
+							callback(post.id);
+							break;
+						case 'time':
+							callback(post.stamp);
+							break;
+						case 'title':
+							callback(post.headline);
+							break;
+						case 'link':
+							callback(post.url);
+							break;
+						case 'text':
+							callback(post.copy);
+					}
+				} else {
+					callback('');
+				}
+			} else {
+				callback('');
+			}
+		});
+		r.addEventListener('error', function() {callback('')});
+		r.open('get', jsonurl, true);
+		r.send();
+	}
+	
+	ext.news2 = function(callback) {
+		var jsonurl = 'https://api.scratch.mit.edu/news?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
+		var r = new XMLHttpRequest();
+		r.addEventListener('load', function() {
+			if (r.responseText) {
+				var obj = JSON.parse(r.responseText);
+				callback(obj.length);
+			} else {
+				callback('');
+			}
+		});
+		r.addEventListener('error', function() {callback('')});
+		r.open('get', jsonurl, true);
+		r.send();
+	}
+	
 	ext.userUsername = function(stuff, user, callback) {
 		var jsonurl = 'https://api.scratch.mit.edu/users/' + user + '?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
 		var r = new XMLHttpRequest();
 		r.addEventListener('load', function() {
 			if (r.responseText) {
 				var obj = JSON.parse(r.responseText);
-				switch(stuff) {
+				switch (stuff) {
 					case 'id':
 						callback(obj.id);
 						break;
@@ -47,22 +119,6 @@ new (function() {
 					case 'what I\'m working on':
 						callback(obj.profile.status);
 				}
-			} else {
-				callback('');
-			}
-		});
-		r.addEventListener('error', function() {callback('')});
-		r.open('get', jsonurl, true);
-		r.send();
-	}
-	
-	ext.userUsernameMessagesCount = function(user, callback) {
-		var jsonurl = 'https://api.scratch.mit.edu/users/' + user + '/messages/count?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
-		var r = new XMLHttpRequest();
-		r.addEventListener('load', function() {
-			if (r.responseText) {
-				var obj = JSON.parse(r.responseText);
-					callback(obj.count);
 			} else {
 				callback('');
 			}
