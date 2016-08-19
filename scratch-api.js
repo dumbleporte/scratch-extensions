1ce9a634fe30ab394d608e4efb7fad9111218242
dumbleporte <pierrepirate@laposte.net>
2016-08-19 03:39:33 +0200

    Update scratch-api.js

--- scratch-api.js
+++ scratch-api.js
@@ -1,39 +1,112 @@
-/* Extension demonstrating a blocking reporter block */
-/* Sayamindu Dasgupta <sayamindu@media.mit.edu>, May 2014 */
-
-
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @dumbleporte
+ Watch 1
+  Star 0
+  Fork 1 savaka2/scratch-extensions
+ Code  Issues 0  Pull requests 0  Wiki  Pulse  Graphs
+Branch: gh-pages Find file Copy pathscratch-extensions/github-status-api.js
+f63c2ed  17 days ago
+@savaka2 savaka2 why did I ever do that
+1 contributor
+RawBlameHistory     94 lines (88 sloc)  2.63 KB
 new (function() {
-    var ext = this;
-
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'https://stats.epicube.fr/player/'+location+'.json?with=stats',
-              dataType: 'jsonp',
-              success: function( player_id ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = player_id['main']['temp'];
-                  callback(temperature);
-              }
-        });
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
+	var ext = this;
+	var descriptor = {
+		blocks: [
+			['R', 'current system %m.apistatus', 'currentstatus', 'status'],
+			['R', '%m.apilastmessage of last message', 'lastmessage', 'status'],
+			['-'],
+			['r', '%m.timestuff of ISO 8601 timestamp %s', 'isodate', 'date', '2012-12-07T18:11:55Z']
+		],
+		menus: {
+			apistatus: ['status', 'status timestamp'],
+			apilastmessage: ['status', 'text', 'timestamp'],
+			timestuff: ['year', 'month', 'date', 'hour', 'minute', 'second']
+		},
+		url: 'https://github.com/savaka2/scratch-extensions/wiki/GitHub-Status-API'
+	};
+	
+	ext._shutdown = function() {};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'};
+	};
+	
+	ext.currentstatus = function(stuff, callback) {
+		var jsonurl = 'https://crossorigin.me/https://status.github.com/api/status.json?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
+		var r = new XMLHttpRequest();
+		r.addEventListener('load', function() {
+			if (r.responseText) {
+				var obj = JSON.parse(r.responseText);
+				switch(stuff) {
+					case 'status':
+						callback(obj.status);
+						break;
+					case 'status timestamp':
+						callback(obj.last_updated);
+				}
+			} else {
+				callback('');
+			}
+		});
+		r.addEventListener('error', function() {callback('')});
+		r.open('get', jsonurl, true);
+		r.send();
+	} // Credit to Zatnik
+	
+	ext.lastmessage = function(stuff, callback) {
+		var jsonurl = 'https://crossorigin.me/https://status.github.com/api/last-message.json?randomstuff=' + Math.floor(Math.random() * 10000) + '&paranoia=' + Number(new Date()).toString();
+		var r = new XMLHttpRequest();
+		r.addEventListener('load', function() {
+			if (r.responseText) {
+				var obj = JSON.parse(r.responseText);
+				switch(stuff) {
+					case 'status':
+						callback(obj.status);
+						break;
+					case 'text':
+						callback(obj.body);
+						break;
+					case 'timestamp':
+						callback(obj.created_on);
+				}
+			} else {
+				callback('');
+			}
+		});
+		r.addEventListener('error', function() {callback('')});
+		r.open('get', jsonurl, true);
+		r.send();
+	} // Credit to Zatnik
+	
+	ext.isodate = function(stuff, datettime) {
+		switch(stuff) {
+			case 'year':
+				return Number(datettime.substring(0,4));
+				break;
+			case 'month':
+				return Number(datettime.substring(5,7));
+				break;
+			case 'date':
+				return Number(datettime.substring(8,10));
+				break;
+			case 'hour':
+				return Number(datettime.substring(11,13));
+				break;
+			case 'minute':
+				return Number(datettime.substring(14,16));
+				break;
+			case 'second':
+				return Number(datettime.substring(17,19));
+		}
+	}
+	
+	ScratchExtensions.register('GitHub Status API', descriptor, ext);
 })();
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
